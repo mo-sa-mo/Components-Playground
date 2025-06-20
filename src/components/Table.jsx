c914ce8d9c52eb280ea67ab05e6ebcf23cadbe6d
mo-sa-mo <mohamedsabirr1999@gmail.com>
2025-06-20 22:50:44 +0300

    Add data sorting to the column table

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,24 +1,31 @@
 import React from "react";
+import { Fragment } from "react";
 
 function Table({ data, config, keyFn }) {
   const renderedHeaders = config.map((column) => {
-    return <th key={column.lable}>{column.lable}</th>;
+    if (column.header) {
+      return <Fragment key={column.label}>{column.header()}</Fragment>;
+    }
+
+    return <th key={column.label}>{column.label}</th>;
   });
 
   const renderedRows = data.map((rowData) => {
     const renderedCells = config.map((column) => {
       return (
-        <td className="p-2" key={column.lable}>
+        <td className="p-2" key={column.label}>
           {column.render(rowData)}
         </td>
       );
     });
+
     return (
       <tr className="border-b" key={keyFn(rowData)}>
         {renderedCells}
       </tr>
     );
   });
+
   return (
     <table className="table-auto border-spacing-2">
       <thead>
--- src/pages/TablePage.jsx
+++ src/pages/TablePage.jsx
@@ -1,47 +1,39 @@
 import React from "react";
-import Table from "../components/Table";
+// import Table from "../components/Table";
+import SortableTable from "../components/SortableTable";
 
 function TablePage() {
   const data = [
-    {
-      name: "Orange",
-      color: "bg-orange-500",
-      score: 5,
-    },
-    {
-      name: "Apple",
-      color: "bg-red-500",
-      score: 3,
-    },
-    {
-      name: "Banana",
-      color: "bg-yellow-500",
-      score: 1,
-    },
-    {
-      name: "Lime",
-      color: "bg-green-500",
-      score: 4,
-    },
+    { name: "Orange", color: "bg-orange-500", score: 5 },
+    { name: "Apple", color: "bg-red-500", score: 3 },
+    { name: "Banana", color: "bg-yellow-500", score: 1 },
+    { name: "Lime", color: "bg-green-500", score: 4 },
   ];
 
   const config = [
-    { lable: "Name", render: (fruit) => fruit.name },
     {
-      lable: "Color",
-      render: (fruit) => <div className={`p-3 ${fruit.color}`} />,
+      label: "Name",
+      render: (fruit) => fruit.name,
+      sortValue: (fruit) => fruit.name,
     },
     {
-      lable: "Score",
+      label: "Color",
+      render: (fruit) => <div className={`p-3 m-2 ${fruit.color}`} />,
+    },
+    {
+      label: "Score",
       render: (fruit) => fruit.score,
+      sortValue: (fruit) => fruit.score,
     },
   ];
+
   const keyFn = (fruit) => {
     return fruit.name;
   };
+
   return (
     <div>
-      <Table data={data} config={config} keyFn={keyFn} />
+      <SortableTable data={data} config={config} keyFn={keyFn} />
     </div>
   );
 }
