bfd4a7b0158b05b06c1167905c7bb239985afef1
mo-sa-mo <mohamedsabirr1999@gmail.com>
2025-10-02 16:43:53 +0300

    make changes for GitHub pages

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
+  base: "/Components-Playground/", // ضع هنا اسم الريبو بالظبط
   plugins: [tailwindcss()],
 });
